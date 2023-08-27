724fc84986c577695f578a07cba318178c2b0b12
Mayel de Borniol <git@mayel.space>
2023-08-27 16:27:17 +0100

    Add an Enumerable

--- lib/paginator/page.ex
+++ lib/paginator/page.ex
@@ -15,3 +15,20 @@ defmodule Paginator.Page do
 
   defstruct [:page_info, :edges]
 end
+defimpl Enumerable, for: Paginator.Page do
+
+  def reduce(%{edges: edges}, acc, fun) do
+    Enumerable.reduce(edges, acc, fun)
+  end
+
+  def member?(%{edges: edges}, el), do: Enumerable.member?(edges, el)
+
+  def count(page) do
+    {:ok, page.page_info.page_count || length(page.edges)}
+  end
+
+  # def slice(%{edges: edges}) do
+  #   Enumerable.member?(edges)
+  # end
+
+end
